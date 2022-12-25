62a9435c2f69450d1f84ee7b3067a9119456f33a
aklyp <aklypa@gmail.com>
2022-12-25 19:57:35 +0300

    folder size code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.io.File;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String folderPath = "C:\\Users\\aklyp\\OneDrive\\Рабочий стол\\Новая папка";
+        File file = new File(folderPath);
+
+        System.out.println(file.length());
+
+        System.out.println(getFolderSize(file));
+
+    }
+
+    public static long getFolderSize(File folder) {
+        if (folder.isFile()) {
+            return folder.length();
+        }
+        long sum = 0;
+        File[] files = folder.listFiles();
+        assert files != null;
+        for (File file : files) {
+            sum += getFolderSize(file);
+        }
+        return sum;
     }
 }
